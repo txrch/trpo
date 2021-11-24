582d39cb2f48e69797b0ad974f1ec367bbaee686
txrch <90940845+txrch@users.noreply.github.com>
2021-11-24 18:55:37 +0300

    Add files via upload

---
+++ trpo5(task1,2)/trpo5.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+int x;
+
+void IntToString(int d, string s, char c)
+{
+	while (x != 0) {
+
+		d = x % 10;
+		x = x / 10;
+		c = (char)(d + 48);
+		s = c + s;
+	}
+	cout << "Строка: " << s << endl;
+}
+
+void IntToString(int d, string s, char c, int t)
+{
+	cout << "Введите количество элементов для перевода: "; cin >> t;
+
+	for (int i = 0; i < t; i++) {
+
+		d = x % 10;
+		x = x / 10;
+		c = (char)(d + 48);
+		s = c + s;
+	}
+	cout << "Строка: " << s << endl;
+}
+
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	int d = 0, t = 0;
+	string s = "";
+	char c = ' ';
+
+	cout << "Число:"; cin >> x;
+	cout << "nomer 1" << endl;
+	IntToString(d, s, c);
+
+	cout << endl << "Число:"; cin >> x;
+	cout << "nomer 2" << endl;
+	IntToString(d, s, c, t);
+
+	return 0;
+}
+
---
+++ trpo5(task3)/trpo5(task3).cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <time.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	srand(time(0));
+
+	int size;
+	cout << "Введите размер массива: "; cin >> size;
+
+	int* arr = new int[size];
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 9;
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	int num, count = 0;
+	cout << "Введите число: "; cin >> num;
+
+	for (int i = 0; i < size; i++) {
+		if (arr[i] == num)
+			count++;
+	}
+	cout << "Число " << num << " встречается " << count;
+	if (count == 2 || count == 3 || count == 4)
+		cout << " раза." << endl;
+	else
+		cout << " раз." << endl;
+
+	delete[] arr;
+
+	return 0;
+}
